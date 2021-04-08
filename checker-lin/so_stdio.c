7f4b94b10e5b587720ed71e676dfcdc40ec0b123
filimonadrian <adrian.filimon@hotmail.com>
2021-04-08 22:31:25 +0300

    Solved first task - so_open_file

---
+++ checker-lin/so_stdio.c
@@ -0,0 +1,124 @@
+#include "so_stdio.h"
+
+SO_FILE *so_fopen(const char *pathname, const char *mode)
+{
+	SO_FILE *stream = malloc(sizeof(SO_FILE));
+	int fd = 0;
+
+	if (stream == NULL)
+		exit(ENOMEM);
+
+	memset(stream, 0, sizeof(SO_FILE));
+
+	if (!strcmp(mode, "r+"))
+		fd = open(pathname, O_RDWR);
+	else if (!strcmp(mode, "w+"))
+		fd = open(pathname, O_RDWR | O_CREAT | O_TRUNC);
+	else if (!strcmp(mode, "a+"))
+		fd = open(pathname, O_APPEND | O_RDONLY | O_CREAT);
+	else if (!strcmp(mode, "r"))
+		fd = open(pathname, O_RDONLY);
+	else if (!strcmp(mode, "w"))
+		fd = open(pathname, O_WRONLY | O_CREAT | O_TRUNC);
+	else if (!strcmp(mode, "a"))
+		fd = open(pathname, O_APPEND | O_CREAT);
+	else {
+		free(stream);
+		return NULL;
+	}
+
+	if (fd <= 0){
+		free(stream);
+		return NULL;
+	}
+
+	stream->fd = fd;
+
+	return stream;
+}
+
+int so_fclose(SO_FILE *stream)
+{
+	int ret = 0;
+	int fd = 0;
+
+	if (stream == NULL)
+		return SO_EOF;
+
+	fd = stream->fd;
+	free(stream);
+
+	if (fd < 0)
+		return SO_EOF;
+
+	ret = close(fd);
+	if (ret != 0)
+		return SO_EOF;
+
+	return 0;
+}
+
+
+int so_fgetc(SO_FILE *stream)
+{
+	return 0;
+}
+
+int so_fputc(int c, SO_FILE *stream)
+{
+	return 0;
+}
+
+size_t so_fread(void *ptr, size_t size, size_t nmemb, SO_FILE *stream)
+{
+	return 0;
+}
+
+size_t so_fwrite(const void *ptr, size_t size, size_t nmemb, SO_FILE *stream)
+{
+	return 0;
+}
+
+int so_fseek(SO_FILE *stream, long offset, int whence)
+{
+	return 0;
+}
+
+long so_ftell(SO_FILE *stream)
+{
+	return 0;
+}
+
+int so_fflush(SO_FILE *stream)
+{
+	return 0;
+}
+
+int so_fileno(SO_FILE *stream)
+{
+	if (stream != NULL)
+		return stream->fd;
+
+	return -1;
+}
+
+int so_feof(SO_FILE *stream)
+{
+	return 0;
+}
+
+int so_ferror(SO_FILE *stream)
+{
+	return 0;
+}
+
+SO_FILE *so_popen(const char *command, const char *type)
+{
+	return NULL;
+}
+
+int so_pclose(SO_FILE *stream)
+{
+	return 0;
+}
+
---
+++ checker-lin/so_stdio.h
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdint.h>
+#include <string.h>
+#include "../util/so_stdio.h"
+
+#define BUFSIZE 4096
+
+typedef struct _so_file {
+	char buffer[4096];
+	int fd;
+
+} SO_FILE;
